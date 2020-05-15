9802afa50ec9bd4014ecf63f336133346235a36e
Mike Cossalter <mikecossalter@yahoo.com>
2020-05-15 09:03:16 -0500

    inital setup

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import { connect } from 'react-redux'
+
+class App extends Component {
+  // Renders the entire app on the DOM
+  render() {
+    return (
+      <div className="App">
+        <p>Empty Page</p>
+      </div>
+    );
+  }
+}
+
+
+const putStateOnProps = (reduxState) => ({ reduxState })
+export default connect(putStateOnProps)(App);
